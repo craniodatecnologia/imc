5ae71a6ccc7a0326ff01281fe8d00715365a20bd
craniodatecnologia <craniodatecnologia@gmail.com>
2018-01-24 19:58:30 -0200

    Initial commit

---
+++ app/src/main/java/br/com/craniodatecnologia/imc/MainActivity.java
@@ -0,0 +1,14 @@
+package br.com.craniodatecnologia.imc;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
